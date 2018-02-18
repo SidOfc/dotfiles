4484b0f384139b0f13c83bf0409c89a61e6766c5
Sidney Liebrand <sidneyliebrand@gmail.com>
2018-02-18 11:04:22 +0100

    fix git info for new repo's without any commits

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,19 +1,23 @@
 function fish_prompt --description 'Write out the prompt'
   set -l __stat $status
+
   switch $__stat
     case 0   ; set __stat_color 'green'
     case 127 ; set __stat_color 'yellow'
     case '*' ; set __stat_color 'red'
   end
 
-  if not set -q __git_cb
-    if test (git status 2>/dev/null ^&1 | tail -1 | cut -f1 -d',') = 'nothing to commit'
-      set __git_dirty (set_color green) '⚑'
-    else
+  if test (git rev-parse --git-dir 2>/dev/null)
+    set __gst (git status -s)
+    set __gbr (git status | head -1 | string split ' ')[-1]
+
+    if test -n "$__gst"
       set __git_dirty (set_color magenta) '⚑'
+    else
+      set __git_dirty (set_color green) '⚑'
     end
 
-    set __git_cb (set_color yellow)" on "(set_color green)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)"$__git_dirty"
+    set __git_cb (set_color yellow)" on "(set_color green)"$__gbr"(set_color normal)"$__git_dirty"
   end
 
   echo -n (set_color $__stat_color)'⬤'(set_color blue) (prompt_pwd)"$__git_cb"(set_color yellow) '❯ '
