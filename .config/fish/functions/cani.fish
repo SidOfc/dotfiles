653c0331b3bd8a3b6fb5fbff0531f038e7eb5b12
Sidney Liebrand <sidneyliebrand@gmail.com>
2018-07-29 22:29:20 +0200

    Replace cani fish script with ruby gem

--- .config/fish/functions/cani.fish
+++
@@ -1,4 +0,0 @@
-function cani --description "Caniuse support tables"
-  set -l feat (ciu | sort -rn | eval "fzf $FZF_DEFAULT_OPTS --ansi --header='[caniuse:features]'" | sed -e 's/^.*%\ *//g' | sed -e 's/   .*//g')
-end
-
