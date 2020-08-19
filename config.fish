c3a39e017d2f991dd0428e0175b32bde7a607e60
Sidney Liebrand <sidneyliebrand@gmail.com>
2020-08-19 12:46:11 +0200

    source proper asdf.fish file in config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 # Settings {{{
   set fish_greeting
-  set -U  fish_user_paths            /usr/local/opt/openssl/bin $HOME/bin $HOME/.asdf/shims $HOME/.asdf/bin $HOME/.fzf/bin
+  set -U  fish_user_paths            /usr/local/opt/openssl/bin $HOME/bin $HOME/.asdf/asdf.fish $HOME/.fzf/bin
   set -gx FZF_DEFAULT_OPTS           '--height=50% --min-height=15 --reverse'
   set -gx FZF_DEFAULT_COMMAND        'rg --files --no-ignore-vcs --hidden'
   set -gx FZF_CTRL_T_COMMAND         $FZF_DEFAULT_COMMAND
